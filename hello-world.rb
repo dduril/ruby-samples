a55cb1d1223f6842902c7a33db43c201c468c8c6
Doug Duril <doug.duril@gmail.com>
2016-07-01 14:08:09 -0700

    Adding first Ruby example

--- hello-world.rb
+++
@@ -1,6 +0,0 @@
-# print a simple message.
-
-# we do this because we are required when starting with a new programming language or environment.
-# we also do this because it is verification that our environment is in fact correctly configured.
-
-puts "Hello World!"
